6e0c164bad15354f84f67386f41cdeca0c5ac128
RatnaTriwish <103371791+RatnaTriwish@users.noreply.github.com>
2024-09-23 15:29:06 +0530

    exception

--- src/main/java/exceptions/Exception1.java
+++ src/main/java/exceptions/Exception1.java
@@ -3,13 +3,12 @@ package exceptions;
 public class Exception1 {
 
     public static void main(String[] args){
-
-
+        demoArrayException();
     }
 
     public static void demoArrayException(){
 
         String[] data = {"s1","s2","s3","s4"};
-        System.out.println("The result is:" +data);
+        System.out.println("The result is:" +data[10]);
     }
 }
